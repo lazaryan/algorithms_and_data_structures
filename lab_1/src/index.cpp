c178e006a2cfee2be8f8076a3dcd6718904f8b8e
lazaryan <lazaryan99@gmail.com>
2022-02-16 23:31:09 +0300

    fix output

--- lab_1/src/index.cpp
+++ lab_1/src/index.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "Tree/Tree.h"
 #include "CSV/CSVWritter.h"
 
@@ -24,15 +26,29 @@ int main()
 	std::vector<std::string> headers;
 	std::vector<std::vector<std::string>> receipts(size);
 
+	int sum_count_products = 0;
+	int sum_price_receipts = 0;
+
 	for (Tree::Tree::t_receipts::iterator it = receipts_table.begin(); it != receipts_table.end(); ++it)
 	{
 		headers.push_back(it->first);
+
+		bool isPrice = it->first == "Price";
 
 		std::vector<int> counts = it->second;
 
 		for (int j = 0; j < size; j++)
 		{
 			receipts[j].push_back(std::to_string(counts[j]));
+
+			if (isPrice)
+			{
+				sum_price_receipts += counts[j];
+			}
+			else
+			{
+				sum_count_products += counts[j];
+			}
 		}
 	}
 
@@ -42,6 +58,10 @@ int main()
 	{
 		out_writter.write_line(receipts[i]);
 	}
+
+	std::cout << "COUNT: " << size << std::endl;
+	std::cout << "MIDDLE COUNT PRODUCTS: " << sum_count_products / size << std::endl;
+	std::cout << "MIDDLE PRICE: " << (double)sum_price_receipts / size << std::endl;
 
 	return 0;
 }
